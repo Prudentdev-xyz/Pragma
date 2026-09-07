bf2783afbb27be7dce428bd18e808225e5a5cfa3
prudentdev-xyz <olayiwolaabdqoharr226@gmail.com>
2026-09-08 00:04:57 +0100

    fix: remove hardcoded nextjs build paths to fix vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import path from "path";
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
